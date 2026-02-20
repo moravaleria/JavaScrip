20f358abf5c20e380d661251058eae4a08863c5e
Valeria Mora Cardenas <vamorac@unal.edu.co>
2026-02-20 14:58:57 -0500

    funciones

--- archivo.js
+++ archivo.js
@@ -1,10 +1,19 @@
-var variable1 = "Hola, Mundo!";
-console.log(variable1);
-let variable2 = 42;
-console.log(variable2);
-const variable3 = true;
-console.log(variable3);
+// var variable1 = "Hola, Mundo!";
+// console.log(variable1);
+// let variable2 = 42;
+// console.log(variable2);
+// const variable3 = true;
+// console.log(variable3);
 
-for(let i = 0; i < 10i++) {
-    console.log(i);
+// const valor1 = 10;
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+//FUNCIONES
+function tablaMultiplicar(numero){ 
+    for(let i=0;i<10;i++){
+        console.log(numero + " x " + i + " = " + (numero * i));
+    }
 }
